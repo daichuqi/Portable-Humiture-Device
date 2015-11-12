cfb97cdff66c76c8daea4dbf3a6f53cc00ea836c
Charlie Su <dragonsmirth@gmail.com>
2015-11-12 14:40:38 -0500

    added gitignore to ignore gitignore; also deleted duplicate folder app

--- app/client.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-import React from 'react';
-import Router from 'react-router';
-import Routes from './js/components/routes';
-
-Router.run(Routes, Router.HistoryLocation, function(Root) {
-  React.render(<Root />, document.body);
-});
-
